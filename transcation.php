b07c56c3615fa32357cbd52ef4b41b99e27e01ea
himali gandhi <himali.gandhi@triosstudent.com>
2020-04-29 16:02:36 -0400

    first commit

---
+++ transcation.php
@@ -0,0 +1,300 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!--Site Metas-->
+    <meta name="description" content="A boutique non-bank financial services company specializing in providing growth solutions to small and medium-sized business" />
+    <meta name="keywords" content="Financial, CorFinancial,Business, small business, medium business " />
+    <meta name="author" content="" />
+    <meta name="viewport" content="width=device-width, intial-scale=1.0" />
+    <!--Bootstrap-->
+     <link rel="stylesheet" href="css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+   
+    <title>Transcations</title>
+
+    <link rel="stylesheet" href="style.css" />
+   
+    <link rel="stylesheet" href="css/footer.css" />
+  
+</head>
+<body>
+    <!--Navigation Start-->
+    <nav class="navbar navbar-expand-sm navbar-dark" style="margin-bottom: 15px;">
+   <div class="container-fluid">
+   <a class="navbar-brand js-scroll-trigger" href="about.php">
+                <img class="img-fluid" src="images/logo2.png" alt="" style="height:80px; margin-left:25px; margin-top:30px;"/>
+            </a>
+            <h3 style="color:black;margin-left:25px;">COMMERCIAL RESTRUCTURING SPECIALISTS</h3>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="navbarsExampleCenteredNav" aria-expanded="false" aria-label="Toggle navigation" style="border-color:#8B040B;background-color: #8B040B;">
+      <span class="navbar-toggler-icon"></span>
+      </button>    
+      <div class="collapse navbar-collapse navbar-collapse justify-content-md-end" id="mainNav">
+        <!-- <h5 style="text-align: right; color:#8B040B; margin-left:27rem;">Finance Application-  </h5> -->
+      <a href="CreditApplication.php" class="sim-btn hvr-bounce-to-top" style="font-size:1em; margin-right:10px;">Finance Application - APPLY NOW</a>
+      <ul class="navbar-nav social-icons">         
+                            <li><a class="facebook" href="https://www.facebook.com/CORFinancial-Corp-118192885553051/"><i class="fa fa-facebook facebook-bg"></i></a></li>
+                            <li><a class="instagram" href="https://www.instagram.com/cor_financial/"><i class="fa fa-instagram"></i></a></li>
+                            <!--<li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>-->
+                            <li><a class="linkedin" href="https://www.linkedin.com/company/corfinancialcorp/"><i class="fa fa-linkedin"></i></a></li>
+            </ul>    
+      </div>
+   </div>
+</nav>
+<nav class="navbar navbar-expand-sm navbar-dark" style="margin-bottom: 15px;">
+   <div class="container-fluid" style="border-top:0.5px solid #44444C; border-bottom: 0.5px solid #44444C;">
+     
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="navbarsExampleCenteredNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>    
+      <div class="collapse navbar-collapse navbar-collapse justify-content-md-center" id="mainNav">
+         <ul class="navbar-nav">
+         <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="index.php">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="LendingSolutions.php">Lending Solutions</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="LendingStructure.php">Lending Structure</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="Industry.php">Industries</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="CORsolutions.php">CORSolutions</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="transcation.php">Transactions</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="blog.php">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="Partnerships.php">Partnerships</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="Contact.php">Contact Us</a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="CreditApplication.php">Finance Application</a>
+                    </li> -->
+                
+         </ul>
+      </div>
+   </div>
+</nav>
+
+    <div class="transactions">
+        <div class="container">
+            <div class="section-title text-center">
+                <h1 style="color:#8B040B;"><u>Recent Transactions</u></h1>
+
+            </div><!-- end title -->
+
+            <div class="row" style="margin-top: 150px;" >
+            <div class="col-md-3" >
+                    <div class="trans-inner-box" >
+
+                        <h2><strong>Power of Sale Purchase</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$6,200,000</h4>
+                        <p>Structured the sale of a property in Power of Sale to a large development company.</p>
+                        <!-- <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" /> -->
+                        <!-- <p>March 2020</p> -->
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3" >
+                    <div class="trans-inner-box" >
+
+                        <h2><strong>Bridge Loan</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$11,200,000</h4>
+                        <p>Faciliy for multinational operations company</p>
+                        <!-- <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" /> -->
+                        <!-- <p>July 2019</p> -->
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+
+                        <h2><strong>Automotive Securitization Loan</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$12,000,000</h4>
+                        <p>Structured and Syndicated Securitization Facility for Automotive Leasing Company </p>
+                        <!-- <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" /> -->
+                        <!-- <p>June 2019</p> -->
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+
+                        <h2><strong>Bridge Loan</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$5,300,000</h4>
+                        <p>Bridge Loan for Entertainment Centre </p>
+                        <!-- <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" /> -->
+                        <!-- <p>May 2019</p> -->
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+
+
+                        <h2><strong>Consolidation and Refinancing Facility</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$25,300,000</h4>
+                        <p>Consolidation of Operating Facilities Including Additional Working Capital for a Multi National Holding Company </p>
+                        <!-- <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" /> -->
+                        <!-- <p>April 2019</p> -->
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+                        <h2><strong>Bridge Loan</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$8,500,000</h4>
+                        <p>To Facilitate Leasehold Improvements and Working Capital </p>
+
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+
+                        <h2><strong>Commercial Purchase</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$1,350,000</h4>
+                        <p>1st Mortgage on a 12,000 sq. ft Commercial Building in Stoney Creek, ON </p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+                        <h2>
+                            <strong>
+                                Pre-eminent Night Club & Recording Studio
+                            </strong>
+                        </h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$6,300,000</h4>
+                        <p>Bridge Loan Pending Long Term Funding</p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+                        <h2><strong>Food Importer</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$750,000</h4>
+                        <p>Letter of Credit Provided to Supplier</p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+                        <h2><strong>Medical Center</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$250,000</h4>
+                        <p>Revolving Facility</p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+                        <h2><strong>Non-Prime Leasing Company</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$1,500,000</h4>
+                        <p>Funds Provided to Refinance and Grow Lease Portfolio</p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+                        <h2><strong>Used Car Dealer</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$2,000,000</h4>
+                        <p>Inventory Line</p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-3">
+                    <div class="trans-inner-box">
+                        <h2><strong>Consumer Products Distributor</strong></h2>
+                        <hr style="width:80px; height:3px; background-color:orangered; margin-top:0px; border-radius:5px;" />
+                        <h4>$250,000</h4>
+                        <p>Revolving Facility</p>
+                    </div>
+                </div><!-- end col -->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div><!-- end section -->
+    <!-- Site footer -->
+    <footer class="footer-section" style="margin-top: 50px;">
+        <div class="container">
+
+            <div class="footer-content pt-5 ">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4 mb-30">
+                        <div class="footer-widget">
+                            <div class="footer-logo" style="margin-top:50px;">
+                                <a href="index.html"><img src="images/logo2.png" class="img-fluid" alt="logo"></a>
+                            </div>
+                        
+
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                        <div class="footer-widget">
+                           
+                            <ul>
+                                <li><a href="index.php">About</a></li>
+                                <li><a href="LendingSolutions.php">Lending Solutions</a></li>
+                                <li><a href="LendingStructure.php">Lending Structure</a></li>
+                                <li><a href="Industry.php">Industry</a></li>
+                                <li><a href="transcation.php">Transcations</a></li>
+                                <li><a href="blog.php">Blog</a></li>
+                                <li><a href="Partnerships.php">Partnerships</a></li>
+                                <li><a href="Contact.php">Contact Us</a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>Contact Us</h3>
+                            </div>
+                            
+                            <p style="color:#8E0414; font-size:1em; margin-bottom:5px;"><i class="fa fa-mobile" aria-hidden="true"></i> &nbsp (416)5462859</p>    
+                            <p style="color:#8E0414; font-size:1em; margin-bottom:5px;"><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:someone@example.com?Subject=Inquired%20Financial%20Solutions" style="color:#8E0414;">info@corfinancialcorp.com</a></p>
+                            <p style="color:#8E0414; font-size:1.5em;"><strong>Stay Connected !!</strong></p>
+                       
+                            <ul class="social-icons">
+                            <li style="width:50px;"><a class="facebook" href="https://www.facebook.com/CORFinancial-Corp-118192885553051/"><i class="fa fa-facebook facebook-bg" style="color:white;"></i></a></li>
+                            <li style="width:50px;"><a class="instagram" href="https://www.instagram.com/cor_financial/"><i class="fa fa-instagram" style="color:white;"></i></a></li>
+                            <!--<li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>-->
+                            <li style="width:100px;"><a class="linkedin" href="https://www.linkedin.com/company/corfinancialcorp/"><i class="fa fa-linkedin" style="color:white;"></i></a></li>
+                        </ul>
+                       
+                    </div>
+                </div>
+            </div>
+            <hr />
+        </div>
+        <div class="copyright-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-6 col-xs-12">
+                        <p class="copyright-text" style="color:#44444C;">
+                            Copyright &copy; 2020 <span style="color:#8B040B;">COR</span>Financial Corp.All Rights Reserved
+                            
+                        </p>
+                    </div>
+                
+
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
